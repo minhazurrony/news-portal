77894afa5b6d3a5dd1d839829b64665dde22ea81
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-28 17:04:39 +0600

    chore: add tailwindcss and flowbite packages

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
