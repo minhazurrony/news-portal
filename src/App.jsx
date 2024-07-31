a897d7b2a939919b72682b8644d7ca2280f56dd7
Minhazur Rahman <minhazur.rony@gmail.com>
2024-07-31 07:40:09 +0200

    feat: render searchbar and news card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,100 @@
-import "./App.css";
+import { Row, Col, message, Spin, Result } from "antd";
+import { getCategoryOptions } from "./utils/getCategoryOptions";
+import { NewsCard, SearchBar } from "./components";
+import { useFetchNews } from "./hooks/useFetchNews";
+import { useState } from "react";
+import { useDebounce } from "./hooks/useDebounce";
+import { useEffect } from "react";
+
+const INITIAL_FORM_VALUES = {
+  keyword: null,
+  date: null,
+  category: null,
+  source: null,
+};
+
+const styles = {
+  spinnerContainer: { minHeight: "50vh" },
+};
 
 function App() {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [formValues, setFormValues] = useState(INITIAL_FORM_VALUES);
+
+  const handleInputChange = (name, value) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onDateChange = (date) => {
+    handleInputChange("date", date ? date.format("YYYY-MM-DD") : null);
+  };
+
+  const onSelectChange = (name, value) => {
+    handleInputChange(name, value);
+    window.localStorage.setItem(name, value);
+  };
+
+  useEffect(() => {
+    const sourceFromLocalStorage = window.localStorage.getItem("source");
+    const categoryFromLocalStorage = window.localStorage.getItem("category");
+    setFormValues((prevState) => ({
+      ...prevState,
+      source: sourceFromLocalStorage,
+      category: categoryFromLocalStorage,
+    }));
+  }, []);
+
+  const debouncedKeyword = useDebounce(formValues.keyword, 1000);
+
+  const { data: news, isLoading } = useFetchNews({
+    keyword: debouncedKeyword,
+    date: formValues.date,
+    category: formValues.category,
+    source: formValues.source,
+  });
+
   return (
     <>
-      <h1>News Portal</h1>
+      {contextHolder}
+      <Row>
+        <Col span={16} offset={4}>
+          <SearchBar
+            handleInputChange={handleInputChange}
+            onDateChange={onDateChange}
+            onSelectChange={onSelectChange}
+            categoryOptions={getCategoryOptions(news)}
+            news={news}
+            formValues={formValues}
+          />
+          <Spin
+            spinning={isLoading}
+            tip="Loading..."
+            size="large"
+            style={styles.spinnerContainer}>
+            <Row gutter={[24, 24]}>
+              {!news.length && !isLoading ? (
+                <Col span={24}>
+                  <Result
+                    title="No data found with this search and filter."
+                    subTitle="Try with different keywords, date, category, source"
+                  />
+                </Col>
+              ) : (
+                news.map((news, idx) => {
+                  return (
+                    <Col span={8} key={idx}>
+                      <NewsCard news={news} isLoading={isLoading} />
+                    </Col>
+                  );
+                })
+              )}
+            </Row>
+          </Spin>
+        </Col>
+      </Row>
     </>
   );
 }
